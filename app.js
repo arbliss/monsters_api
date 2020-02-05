874e6912ccc03eebe3b91722f3425c13ff5175da
Adam Bliss <arbliss@gmail.com>
2020-02-05 17:42:16 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const monsters = require('./routes/monsters');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/monsters', monsters);
+
+app.use((err, req, res, next) => {
+  res.json(err);
+});
+
+module.exports = app;
